560e66a203857c3ce243b6a85cbef8ef578cc6cc
David <83680778+dstosta@users.noreply.github.com>
2022-04-30 17:52:02 -0300

    Add files via upload
    Arquivo com o código da atividade 2 da segunda aula

---
+++ AumentoSalario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2_AumentoSalario
+{
+    internal class AumentoSalario
+    {
+        static void Main(string[] args)
+        {
+            // Declaração de vairaveis
+
+            string funcionario;
+            double salario, salarioreajustado;
+
+            //inicialização de variaveis
+            salarioreajustado = 0;
+
+
+            //logica principal
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Entre com nome do funcionario: ");
+                funcionario = Console.ReadLine();
+
+                Console.WriteLine("Entre com o salario do funcionario: ");
+                salario = Convert.ToDouble(Console.ReadLine());
+
+                if (salario <= 300)
+                {
+                    salarioreajustado = salario * 1.5;
+                }
+
+                if (salario > 300)
+                {
+                    salarioreajustado = salario * 1.3;
+                }
+                Console.WriteLine("O funcionário " + funcionario + " tinha o salario antigo de: R$ " + salario + " o novo salario reajustado é: R$ " + salarioreajustado);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
